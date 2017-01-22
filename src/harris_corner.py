fc3a0b8db5e0a19b2bdf47788b1c52b337c4693e
Paweł <muchap1@ee.pw.edu.pl>
2017-01-22 19:05:42 +0100

    quite a lot of work, basically: * sliding window for moving over an image in different sizes (pyramids) * reading from camera * harris corner detector * haar cascade classifier to detect eyes * klt example of tracking

--- src/harris_corner.py
+++ src/harris_corner.py
@@ -3,21 +3,21 @@
 import cv2
 import numpy as np
 
-# filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\1.jpg'
-# filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\2.jpg'
-filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\3.jpg'
-# filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\4.jpg'
-# filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\5.jpg'
-# filename = 'G:\\zrenice\\bazy\\Webcam\\eyes\\6.jpg'
+# filename = '..\\bazy\\Webcam\\eyes\\1.jpg'
+# filename = '..\\bazy\\Webcam\\eyes\\2.jpg'
+filename = '..\\bazy\\Webcam\\eyes\\3.jpg'
+# filename = '..\\bazy\\Webcam\\eyes\\4.jpg'
+# filename = '..\\bazy\\Webcam\\eyes\\5.jpg'
+# filename = '..\\bazy\\Webcam\\eyes\\6.jpg'
 # img = cv2.imread('../bazy/BioID-FaceDatabase-V1.2/BioID_0293.pgm')
 # img = cv2.imread('../bazy/BioID-FaceDatabase-V1.2/lenna2.jpg')
 # img = cv2.imread('../bazy/eye.jpeg')
 img = cv2.imread(filename)
-# img = cv2.imread("G:\\zrenice\\bazy\\Webcam\\2016-12-17-175909.jpg", 0)
+# img = cv2.imread("..\\bazy\\Webcam\\2016-12-17-175909.jpg", 0)
 # img = cv2.imread('/home/stevens/Obrazy/Webcam/2016-10-27-220504.jpg')
 # img = cv2.imread('/home/stevens/Obrazy/Webcam/2016-10-27-220018.jpg')
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 gray = np.float32(gray)
 dst = cv2.cornerHarris(gray,2,11,0.1)
@@ -25,12 +25,45 @@ dst = cv2.cornerHarris(gray,2,11,0.1)
 # result is dilated for marking the corners, not important
 dst = cv2.dilate(dst,None)
 
-# Threshold for an optimal value, it may vary depending on the image.
-img[dst>0.01*dst.max()]=[0,0,255]
+# create keypoints from dst
+keypoints = np.argwhere(dst > 0.01 * dst.max())
+keypoints = [cv2.KeyPoint(x[1], x[0], 1) for x in keypoints]
 
-without_extension = filename[:-4]
-extension = filename[-4:]
-cv2.imshow('dst',img)
-cv2.imwrite(without_extension + 'harris' + extension, img)
+# now I use ORB to create descriptor from harris keypoints
+orb = cv2.ORB_create()
+addas, descriptor = orb.compute(img, keypoints)
+
+img2 = cv2.drawKeypoints(img, keypoints, None, color=(0,255,0), flags=0)
+cv2.imshow("harris", img2)
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroyAllWindows()
+
+
+def get_harris_orb_descriptor(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 11, 0.1)
+
+    # result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst, None)
+
+    # create keypoints from dst
+    keypoints = np.argwhere(dst > 0.01 * dst.max())
+    keypoints = [cv2.KeyPoint(x[1], x[0], 1) for x in keypoints]
+
+    # now I use ORB to create descriptor from harris keypoints
+    orb = cv2.ORB_create()
+    addas, descriptor = orb.compute(img, keypoints)
+
+    return descriptor
+
+
+def save_harris_on_disk(img, filename):
+    without_extension = filename[:-4]  # save on the disk
+    extension = filename[-4:]
+
+    cv2.imshow('dst', img)
+    cv2.imwrite(without_extension + 'harris' + extension, img)
+
+
--- src/sliding_window.py
+++ src/sliding_window.py
@@ -4,24 +4,33 @@ from helpers import sliding_window
 import time
 import cv2
 
-image = cv2.imread('G:\\zrenice\\bazy\\Webcam\\1.jpg')
+eye = cv2.imread('..\\bazy\\Webcam\\eyes\\3.jpg', 0)
 (winW, winH) = (103, 31)
+orb = cv2.ORB_create()
+kp_eye, eye_descriptor = orb.detectAndCompute(eye, None)  # find key points and computer descriptor
+
+face = cv2.imread('..\\bazy\\Webcam\\6.jpg', 0)
+matches = []
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 # loop over the image pyramid
-for resized in pyramid(image, scale=1.5):
+for resized in pyramid(face, scale=1.5):
     # loop over the sliding window for each layer of the pyramid
     for (x, y, window) in sliding_window(resized, stepSize=15, windowSize=(winW, winH)):
         # if the window does not meet our desired window size, ignore it
         if window.shape[0] != winH or window.shape[1] != winW:
             continue
+        # clone = resized.copy()
 
-        # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-        # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-        # WINDOW
+        kp_window, windows_descriptor = orb.detectAndCompute(window, None)  #
+        match = matcher.match(eye_descriptor, windows_descriptor)
+        # matches.append(match)
+        # match = sorted(match, key=lambda x: x.distance)
 
-        # since we do not have a classifier, we'll just draw the window
-        clone = resized.copy()
-        cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-        cv2.imshow("Window", clone)
-        cv2.waitKey(1)
-        time.sleep(0.025)
+        print match
+        #
+        # cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+        # cv2.imshow("Window", window)
+        # cv2.waitKey(1)
+        # time.sleep(0.025)
+
